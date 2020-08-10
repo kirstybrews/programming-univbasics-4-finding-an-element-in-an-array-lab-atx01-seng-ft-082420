df7bb58ae8d23aaa4417ac75ec1be6adda69e735
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-10 18:18:13 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.index(value_to_find)
+  counter = 0 
+  while counter < array.length do
+    if counter == value_to_find
+      array[counter]
+    else nil
+      counter += 1 
+  end
 end
